78e7b86172b6d38caa67b943fd452491ae3fe63a
yuri-av <arancibiavieirayuri@gmail.com>
2024-08-18 10:15:20 -0300

    Create yuri-av.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/yuri-av.py
@@ -0,0 +1,27 @@
+#https://python.org
+
+#comentario en una linea
+
+"""
+esto es un comentario 
+en varias
+lineas
+"""
+my_variable = "variable" #esto es una variable
+
+#Python no existen constantes
+MY_CONSTANT = "mi constante" #No permite constante pero por convención esto es una constante
+
+#tipos de datos
+
+my_int = 12 #numero entero
+my_float = 1.2
+my_boolean = True
+my_boolean2 = False
+my_string = "string"
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_boolean))
+print(type(my_boolean2))
+print(type(my_string))
